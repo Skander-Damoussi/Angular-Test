ac470e4c276d95cf5eb3547a553be3d47fa9e925
Skander Damoussi <Damoussi.Skander@gmail.com>
2021-05-31 14:41:07 +0200

    added performance test

--- Angular-Performance-Test/src/app/app.component.ts
+++ Angular-Performance-Test/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+
+declare global {
+  interface Window { strum: Function; }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular-Performance-Test';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        window['strum']('routeChange', event.url);
+      }
+    });
+  }
 }
